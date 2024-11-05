e5c3c69e06021004f4feea8f83036458b220fead
Ramon Vicens <ramonacus@gmail.com>
2024-11-05 20:57:03 +0100

    Creado el juego de tutorial de Phaser

--- eslint.config.js
+++ eslint.config.js
@@ -1,39 +1,8 @@
-import eslint from 'eslint';
-import prettier from 'eslint-config-prettier';
-import pluginPrettier from 'eslint-plugin-prettier';
-import pluginTypeScript from '@typescript-eslint/eslint-plugin';
-import parserTypeScript from '@typescript-eslint/parser';
+import globals from 'globals';
+import pluginJs from '@eslint/js';
 
+/** @type {import('eslint').Linter.Config[]} */
 export default [
-  {
-    files: ['**/*.ts', '**/*.tsx', '**/*.js', '**/*.jsx'],
-
-    languageOptions: {
-      ecmaVersion: 'latest',
-      sourceType: 'module',
-      parser: parserTypeScript,
-    },
-
-    plugins: {
-      '@typescript-eslint': pluginTypeScript,
-      prettier: pluginPrettier,
-    },
-
-    rules: {
-      ...eslint.recommended,
-      ...pluginTypeScript.configs.recommended.rules,
-      ...prettier.rules,
-
-      // Custom rules
-      'prettier/prettier': 'error',
-      curly: ['error', 'all'], // Enforce braces around all blocks
-      quotes: ['error', 'single'],
-      semi: ['error', 'always'],
-      '@typescript-eslint/no-unused-vars': [
-        'warn',
-        { argsIgnorePattern: '^_' },
-      ],
-      'linebreak-style': 'off',
-    },
-  },
+  { languageOptions: { globals: globals.browser } },
+  pluginJs.configs.recommended,
 ];
