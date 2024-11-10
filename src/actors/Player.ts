eb9e1f7dd387c4ec18c21c5bc7138eb2ccd0278e
Jordi Ferrer <jferrerbeltran@gmail.com>
2024-11-10 18:00:44 +0100

    Revert packaage-lock changes

--- src/actors/Player.ts
+++ src/actors/Player.ts
@@ -2,8 +2,21 @@ import Phaser, { Scene } from 'phaser';
 
 class Player extends Phaser.Physics.Arcade.Sprite {
   declare body: Phaser.Physics.Arcade.Body;
-  speed = 160;
+  maxSpeed = 200;
+  acceleration = 7.5;
+  deceleration = 10;
+  speed = 0;
+  airborneSpeed = 200;
   jumpSpeed = -520;
+
+  isJumping = false;
+  canCoyoteJump = false;
+  coyoteTime = 150;
+  coyoteTimeTimeout: number;
+
+  jumpBufferTime = 125;
+  jumpBufferTimeout: number;
+  isJumpBuffered = false;
 
   constructor(scene: Scene, x: number, y: number) {
     super(scene, x, y, 'player');
@@ -65,7 +78,7 @@ class Player extends Phaser.Physics.Arcade.Sprite {
       this.flipX = false;
     }
 
-    if (this.body.blocked.down) {
+    if (this.body.onFloor()) {
       this.updateGrounded(cursors);
     } else {
       this.updateAirborne(cursors);
@@ -75,6 +88,10 @@ class Player extends Phaser.Physics.Arcade.Sprite {
   private updateAirborne(cursors: Phaser.Types.Input.Keyboard.CursorKeys) {
     const { x: speedX, y: speedY } = this.body.velocity;
 
+    if (!this.isJumping && this.canCoyoteJump) {
+      this.setCoyoteTimeout();
+    }
+
     if (Math.abs(speedY) < 10) {
       this.play('jump-float', true);
     } else if (speedY > 0) {
@@ -82,31 +99,95 @@ class Player extends Phaser.Physics.Arcade.Sprite {
     } else {
       this.play('jump-up', true);
     }
+
     if (cursors.left.isDown) {
-      this.setVelocityX(-this.speed);
+      this.setVelocityX(-this.airborneSpeed);
     } else if (cursors.right.isDown) {
-      this.setVelocityX(this.speed);
+      this.setVelocityX(this.airborneSpeed);
     } else {
       const newSpeedX = Math.abs(speedX) < 10 ? 0 : speedX * 0.95;
 
       this.setVelocityX(newSpeedX);
     }
+
+    this.handleJump(cursors);
   }
 
   private updateGrounded(cursors: Phaser.Types.Input.Keyboard.CursorKeys) {
+    this.isJumping = false;
+    this.canCoyoteJump = true;
+    this.clearCoyoteTimeout();
+
+    if (this.isJumpBuffered) {
+      this.jump();
+      this.clearJumpBuffer();
+    }
+
     if (cursors.left.isDown) {
+      this.speed = Math.min(this.speed + this.acceleration, this.maxSpeed);
       this.setVelocityX(-this.speed);
       this.play('walk', true);
     } else if (cursors.right.isDown) {
+      this.speed = Math.min(this.speed + this.acceleration, this.maxSpeed);
       this.setVelocityX(this.speed);
       this.play('walk', true);
+    } else if (this.speed > this.deceleration) {
+      const decelerationDirection = this.body.velocity.x < 0 ? -1 : 1;
+      this.speed -= this.deceleration;
+      this.setVelocityX(this.speed * decelerationDirection);
     } else {
+      this.speed = 0;
       this.setVelocityX(0);
       this.play('idle', true);
     }
 
-    if (cursors.up.isDown) {
-      this.setVelocityY(this.jumpSpeed);
+    this.handleJump(cursors);
+  }
+
+  private canJump() {
+    return this.body.onFloor() || this.canCoyoteJump;
+  }
+
+  private handleJump(cursors: Phaser.Types.Input.Keyboard.CursorKeys) {
+    if (cursors.up.isDown && this.canJump()) {
+      this.jump();
+    } else if (cursors.up.isDown && !this.canJump()) {
+      this.bufferJump();
+    }
+  }
+
+  private jump() {
+    this.isJumping = true;
+    this.canCoyoteJump = false;
+    this.clearCoyoteTimeout();
+    this.setVelocityY(this.jumpSpeed);
+  }
+
+  private setCoyoteTimeout() {
+    this.coyoteTimeTimeout = setTimeout(() => {
+      this.canCoyoteJump = false;
+    }, this.coyoteTime);
+  }
+
+  private clearCoyoteTimeout() {
+    if (this.coyoteTimeTimeout) {
+      clearTimeout(this.coyoteTimeTimeout);
+      this.coyoteTimeTimeout = 0;
+    }
+  }
+
+  private bufferJump() {
+    this.isJumpBuffered = true;
+    this.jumpBufferTimeout = setTimeout(() => {
+      this.isJumpBuffered = false;
+    }, this.jumpBufferTime);
+  }
+
+  private clearJumpBuffer() {
+    this.isJumpBuffered = false;
+    if (this.jumpBufferTimeout) {
+      clearTimeout(this.jumpBufferTimeout);
+      this.jumpBufferTimeout = 0;
     }
   }
 }
